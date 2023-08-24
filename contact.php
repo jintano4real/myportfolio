935950292d0b8d3343f7fa779e6ec20091036170
jintano4real <jintano4real007@yahoo.com>
2023-08-24 14:11:08 +0100

    my second commit

--- contact.php
+++ contact.php
@@ -12,38 +12,25 @@
   <meta content="" name="description">
   <meta content="" name="keywords">
 
-  <!-- Favicons -->
   <link href="assets/img/favicon.png" rel="icon">
   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
-  <!-- Vendor CSS Files -->
   <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
   <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
   <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
 
-  <!-- Template Main CSS File -->
   <link href="assets/css/style.css" rel="stylesheet">
 
-  <!-- =======================================================
-  * Template Name: DevFolio
-  * Updated: May 30 2023 with Bootstrap v5.3.0
-  * Template URL: https://bootstrapmade.com/devfolio-bootstrap-portfolio-html-template/
-  * Author: BootstrapMade.com
-  * License: https://bootstrapmade.com/license/
-  ======================================================== -->
 </head>
 
 <body>
 
-  <!-- ======= Header ======= -->
   <header id="header" class="fixed-top">
     <div class="container d-flex align-items-center justify-content-between">
 
       <h1 class="logo"><a href="index.html">Goodmanfolio</a></h1>
-      <!-- Uncomment below if you prefer to use an image logo -->
-      <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-
+      
       <nav id="navbar" class="navbar">
         <ul>
           <li><a class="nav-link scrollto" href="index.html">Home</a></li>
@@ -52,17 +39,13 @@
           <li><a class="nav-link scrollto" href="contact.php">Contact</a></li>
         </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
-      </nav><!-- .navbar -->
+      </nav>
 
     </div>
-  </header><!-- End Header -->
-
-  <!-- ======= Hero Section ======= -->
-  
+  </header>
 
   <main id="main">
 
-        <!-- ======= Contact Section ======= -->
         <section id="contact" class="paralax-mf footer-paralax bg-image sect-mt4 route" style="background-image: url(assets/img/overlay-bg.jpg)">
         <div class="overlay-mf"></div>
       <div class="container">
@@ -132,9 +115,9 @@
                       
                       </p>
                       <ul class="list-ico">
-                        <li><span class="bi bi-geo-alt"></span> NO 7 Rahama Plaza, KM 8 Okene-Lokoja Expressway, Kogi State</li>
-                        <li><span class="bi bi-phone"></span> (234) 379-45052</li>
-                        <li><span class="bi bi-envelope"></span> jintano4real007@gmail.com</li>
+                        <li><span class="bi bi-geo-alt"></span> <b>NO 7 Rahama Plaza, KM 8 Okene-Lokoja Expressway, Kogi State.</b></li>
+                        <li><span class="bi bi-phone"></span> <b>(234) 703-794-5052</b></li>
+                        <li><span class="bi bi-envelope"></span> <b>jintano4real007@gmail.com</b></li>
                       </ul>
                     </div>
                     <div class="socials">
@@ -155,36 +138,29 @@
       
                          
                         
-    </section><!-- End Contact Section -->
+    </section>
 
-  </main><!-- End #main -->
+  </main>
 
-  <!-- ======= Footer ======= -->
   <footer>
     <div class="container">
       <div class="row">
         <div class="col-sm-12">
           <div class="copyright-box">
-            <p class="copyright">&copy; Copyright <strong>Goodmanfolio</strong>. All Rights Reserved</p>
+            <p class="copyright">&copy; Copyright 2023&nbsp <strong>Goodmanfolio</strong>. All Rights Reserved</p>
             <div class="credits">
-              <!--
-              All the links in the footer should remain intact.
-              You can delete the links only if you purchased the pro version.
-              Licensing information: https://bootstrapmade.com/license/
-              Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/buy/?theme=DevFolio
-            -->
-              Designed by JoshDKing
+              
+              <b>Designed by Goodman</b>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </footer><!-- End  Footer -->
+  </footer>
 
   <div id="preloader"></div>
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-  <!-- Vendor JS Files -->
   <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
@@ -192,7 +168,6 @@
   <script src="assets/vendor/typed.js/typed.umd.js"></script>
   <script src="assets/vendor/php-email-form/validate.js"></script>
 
-  <!-- Template Main JS File -->
   <script src="assets/js/main.js"></script>
 
 </body>
